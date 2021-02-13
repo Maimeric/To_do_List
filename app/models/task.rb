87d01155bed2ff618b02e966d06d0fc810642386
Artiom <artiom.levitskiy@gmail.com>
2021-02-14 00:35:55 +0300

    add status in tasks

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,18 @@
 class Task < ApplicationRecord
+  STATUSES = { 'New' => 'New task',
+               'Progress' => 'In progress',
+               'Done' => 'Task done',
+  }
+
+  def self.valid_statuses
+    STATUSES.keys
+  end
+
   validates :title, presence: true
   validates :description, presence: true, length: { minimum: 10 }
+  validates :status, inclusion: { in: valid_statuses }, presence: true
+
+  def extended_status
+    STATUSES[status]
+  end
 end
